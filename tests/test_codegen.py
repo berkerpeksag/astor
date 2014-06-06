5f9ce56eb3106255b53dbf25625156bc834ee280
Berker Peksag <berker.peksag@gmail.com>
2014-06-06 22:39:12 +0300

    Add initial test suite.
    See #3 for details.
    
    Closes #4.

--- tests/test_codegen.py
+++ tests/test_codegen.py
@@ -1,70 +1,52 @@
-#!/usr/bin/env python
 """
 Part of the astor library for Python AST manipulation
 
 License: 3-clause BSD
 
-Copyright 2012 (c) Patrick Maupin
+Copyright 2014 (c) Berker Peksag
 """
 
-
-import fnmatch
-import os
-import sys
 import ast
+import unittest
+import textwrap
 
-sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 import astor
 
 
-def findpy(root):
-    if isinstance(root, str):
-        if os.path.isfile(root):
-            if root.endswith('.py'):
-                yield root
-            return
-        for fname in os.listdir(root):
-            fname = os.path.join(root, fname)
-            if os.path.isdir(fname):
-                for fname in findpy(fname):
-                    yield fname
-            elif fnmatch.fnmatch(fname, '*.py'):
-                yield fname
-    else:
-        for dirname in root:
-            for fname in findpy(dirname):
-                yield fname
+class CodegenTestCase(unittest.TestCase):
 
-def testone(fname, f1=None, f2=None):
-    try:
-        ast1 = astor.parsefile(fname)
-    except (SyntaxError, UnicodeDecodeError):
-        print("IGNORED %s" % fname)
-        return
-    dump1 = astor.dump(ast1)
-    reconstitute = '# -*- coding: utf-8 -*-\n' + astor.to_source(ast1)
-    ast2 = ast.parse(reconstitute, fname)
-    dump2 = astor.dump(ast2)
-    ok = dump1 == dump2
-    print('%-8s%s' % ('OK' if dump1 == dump2 else 'FAIL', fname))
-    if not ok and f1 is not None and f2 is not None:
-        f1.write('\n\n***************************************\n%s\n***************************************\n\n\n' % fname)
-        f2.write('\n\n***************************************\n%s\n***************************************\n\n\n' % fname)
-        f1.write(dump1)
-        f2.write(dump2)
-        f = open('bad.txt', 'w')
-        f.write(reconstitute)
-        f.close()
-    return ok
+    def assertAstSourceEqual(self, source):
+        self.assertEqual(astor.to_source(ast.parse(source)), source)
 
+    def test_imports(self):
+        source = "import ast"
+        self.assertAstSourceEqual(source)
+        source = "import operator as op"
+        self.assertAstSourceEqual(source)
+        source = "from math import floor"
+        self.assertAstSourceEqual(source)
 
-def go(root, stoponfail=True, f1=None, f2=None):
-    for fname in findpy(root or os.path.dirname(fnmatch.__file__)):
-        if not testone(fname, f1, f2) and stoponfail:
-            break
+    def test_try_expect(self):
+        source = textwrap.dedent("""\
+        try:
+            'spam'[10]
+        except IndexError:
+            pass""")
+        self.assertAstSourceEqual(source)
+
+        source = textwrap.dedent("""\
+        try:
+            'spam'[10]
+        except IndexError as exc:
+            sys.stdout.write(exc)""")
+        self.assertAstSourceEqual(source)
+
+    def test_del_statement(self):
+        source = "del l[0]"
+        self.assertAstSourceEqual(source)
+        source = "del obj.x"
+        self.assertAstSourceEqual(source)
 
 
 if __name__ == '__main__':
-    f1 = open('test_codegen_dump_1.txt', 'w')
-    f2 = open('test_codegen_dump_2.txt', 'w')
-    go(sys.argv[1:], False, f1, f2)
+    unittest.main()
