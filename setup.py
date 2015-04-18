a463ac8ae112dd19bfc1c8e2df170023114ded07
Berker Peksag <berker.peksag@gmail.com>
2015-04-19 01:04:52 +0300

    Remove 3.5 for now. It's not added to PyPI yet.

--- setup.py
+++ setup.py
@@ -30,7 +30,6 @@ setup(
         'Programming Language :: Python :: 3.2',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: Implementation',
         'Programming Language :: Python :: Implementation :: CPython',
         'Programming Language :: Python :: Implementation :: PyPy',
