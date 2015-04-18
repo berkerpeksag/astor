bb29d32c8b2b58c025a72a378f7050b1baca0c10
Zack M. Davis <code@zackmdavis.net>
2015-04-18 13:28:03 -0700

    matrix mult. codegen test; try to use unittest2

--- tests/test_misc.py
+++ tests/test_misc.py
@@ -1,22 +1,20 @@
 import ast
-import unittest
 import sys
+
+try:
+    import unittest2 as unittest
+except ImportError:
+    import unittest
 
 import astor
 
 
 class GetSymbolTestCase(unittest.TestCase):
 
+    @unittest.skipUnless(sys.version_info >= (3, 5),
+                         "ast.MatMult introduced in Python 3.5")
     def test_get_mat_mult(self):
-        if sys.version_info < (2, 7):
-            # We can't use `@unittest.skipIf` or `raise skipTest` in
-            # Python 2.6
-            pass
-        elif sys.version_info < (3, 5):
-            raise unittest.SkipTest("ast.MatMult introduced in Python 3.5")
-        else:
-            self.assertEqual('@', astor.misc.get_binop(ast.MatMult()))
-
+        self.assertEqual('@', astor.misc.get_binop(ast.MatMult()))
 
 if __name__ == '__main__':
     unittest.main()
