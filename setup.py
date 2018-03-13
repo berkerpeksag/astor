9d2eac5c737ffc4d94efe5439abacb8ccc0ebb1a
KOLANICH <kolan_n@mail.ru>
2018-03-13 11:47:58 +0300

    Started using setup.cfg instead of setup.py for metadata storage.

--- setup.py
+++ setup.py
@@ -1,39 +1,15 @@
-from setuptools import setup, find_packages
+import sys
+import os
+from setuptools import setup
+from setuptools.config import read_configuration
+from setuputils import find_version
+this_dir = os.path.dirname(__file__)
 
-from setuputils import find_version, read
+def here(*paths):
+    return os.path.join(this_dir, *paths)
 
-
-setup(
-    name='astor',
-    version=find_version('astor/__init__.py'),
-    description='Read/rewrite/write Python ASTs',
-    long_description=read('README.rst'),
-    author='Patrick Maupin',
-    author_email='pmaupin@gmail.com',
-    platforms='Independent',
-    url='https://github.com/berkerpeksag/astor',
-    packages=find_packages(exclude=['tests']),
-    py_modules=['setuputils'],
-    classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'Environment :: Console',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.6',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: Implementation',
-        'Programming Language :: Python :: Implementation :: CPython',
-        'Programming Language :: Python :: Implementation :: PyPy',
-        'Topic :: Software Development :: Code Generators',
-        'Topic :: Software Development :: Compilers',
-    ],
-    keywords='ast, codegen, PEP8',
-)
+config = read_configuration(here('setup.cfg'))
+config['metadata']['version'] = find_version(here('astor', '__init__.py'))
+config['options'].update(config['metadata'])
+config = config['options']
+setup(**config)
