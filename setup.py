f40d9a0c902756baf9cc0b670b542399d47d517d
Berker Peksag <berker.peksag@gmail.com>
2018-07-06 10:50:16 +0300

    Use more Pythonic style in setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,17 @@
-import sys
 import os
+import sys
+
 from setuptools import setup
 from setuptools.config import read_configuration
+
 from setuputils import find_version
-this_dir = os.path.dirname(__file__)
+
 
 def here(*paths):
-    return os.path.join(this_dir, *paths)
+    return os.path.join(os.path.dirname(__file__), *paths)
 
 config = read_configuration(here('setup.cfg'))
 config['metadata']['version'] = find_version(here('astor', '__init__.py'))
 config['options'].update(config['metadata'])
-config = config['options']
-setup(**config)
+
+setup(**config['options'])
