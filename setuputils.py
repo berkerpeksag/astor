12b6a47fefbc9b482920be0a40c2eea96164069c
foreignmeloman <foreignmeloman@gmail.com>
2020-03-17 04:04:34 +0400

    Delete unused setuputils.py

--- setuputils.py
+++
@@ -1,9 +0,0 @@
-import codecs
-import os.path
-
-
-def read(*parts):
-    file_path = os.path.join(os.path.dirname(__file__), *parts)
-    with codecs.open(file_path, 'r') as fobj:
-        content = fobj.read()
-    return content
